b971e9eb1340bcfcd0733b46cf879b80139ab596
jeparlefrancais <jeparlefrancais21@gmail.com>
2023-04-27 13:13:32 -0400

    Upgrade site to latest gatsby and resolve security warnings

--- site/darklua-wasm/src/lib.rs
+++ site/darklua-wasm/src/lib.rs
@@ -4,12 +4,6 @@ use darklua_core::{Configuration, Options, Resources};
 use utils::set_panic_hook;
 use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen]
 pub fn process_code(code: &str, opt_config: JsValue) -> Result<String, JsValue> {
     set_panic_hook();
