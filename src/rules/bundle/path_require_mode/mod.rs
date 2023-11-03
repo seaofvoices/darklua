229c92255fb7078ca68f919577f384046f1ab7b8
jeparlefrancais <jeparlefrancais21@gmail.com>
2023-11-03 13:18:41 -0400

    Refactor bundling to use cached function calls to load modules

--- src/rules/bundle/path_require_mode/mod.rs
+++ src/rules/bundle/path_require_mode/mod.rs
@@ -343,6 +343,24 @@ pub(crate) fn process_block(
     options: &BundleOptions,
     path_require_mode: &PathRequireMode,
 ) -> Result<(), String> {
+    if options.parser().is_preserving_tokens() {
+        log::trace!(
+            "replacing token references of {}",
+            context.current_path().display()
+        );
+        let replace_tokens = ReplaceReferencedTokens::default();
+
+        let apply_replace_tokens_timer = Timer::now();
+
+        replace_tokens.flawless_process(block, context);
+
+        log::trace!(
+            "replaced token references for `{}` in {}",
+            context.current_path().display(),
+            apply_replace_tokens_timer.duration_label()
+        );
+    }
+
     let mut processor = RequirePathProcessor::new(context, options, path_require_mode);
     ScopeVisitor::visit_block(block, &mut processor);
     processor.apply(block, context)
