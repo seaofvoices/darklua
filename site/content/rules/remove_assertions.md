bdfde48f073a6b01ae55e1ea61b94ea334808268
jeparlefrancais <jeparlefrancais21@gmail.com>
2023-11-26 18:07:11 -0500

    Add rule to remove assert calls

---
+++ site/content/rules/remove_assertions.md
@@ -0,0 +1,13 @@
+---
+description: Removes call to the assert function
+added_in: "unreleased"
+parameters:
+  - name: preserve_arguments_side_effects
+    type: boolean
+    description: Defines how darklua handle arguments passed to the function. If true, darklua will inspect each argument and preserve any potential side effects. When false, darklua will not perform any verification and simply erase any arguments passed.
+    default: true
+examples:
+  - content: assert(condition, 'condition is incorrect!')
+---
+
+This rule removes all function calls to `assert`.
---
+++ tests/rule_tests/remove_assertions.rs
@@ -0,0 +1,45 @@
+use darklua_core::rules::{RemoveAssertions, Rule};
+
+test_rule!(
+    remove_debug_profiling,
+    RemoveAssertions::default(),
+    remove_variable_condition("assert(condition)") => "do end",
+    remove_variable_condition_with_message("assert(condition, 'message')") => "do end",
+    remove_function_call_condition("assert(validate(value))") => "do local _ = validate(value) end",
+    remove_variable_condition_with_function_call_message("assert(condition, formatter(condition))") => "do local _ = formatter(condition) end",
+);
+
+test_rule!(
+    remove_debug_profiling_without_side_effects,
+    json5::from_str::<Box<dyn Rule>>(
+        r#"{
+        rule: 'remove_assertions',
+        preserve_arguments_side_effects: false,
+    }"#,
+    )
+    .unwrap(),
+    remove_variable_condition("assert(condition)") => "do end",
+    remove_variable_condition_with_message("assert(condition, 'message')") => "do end",
+    remove_function_call_condition("assert(validate(value))") => "do end",
+    remove_variable_condition_with_function_call_message("assert(condition, formatter(condition))") => "do end",
+);
+
+test_rule_without_effects!(
+    RemoveAssertions::default(),
+    assert_function_used("local function assert() end assert('label')"),
+);
+
+#[test]
+fn deserialize_from_object_notation() {
+    json5::from_str::<Box<dyn Rule>>(
+        r#"{
+        rule: 'remove_assertions',
+    }"#,
+    )
+    .unwrap();
+}
+
+#[test]
+fn deserialize_from_string() {
+    json5::from_str::<Box<dyn Rule>>("'remove_assertions'").unwrap();
+}
