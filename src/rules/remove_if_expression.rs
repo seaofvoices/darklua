babab9829e55868065f23117d8300577e5a30afa
jiwonz <jiwonz0113@gmail.com>
2024-08-30 14:46:28 +0900

    Implement the new translation method and formated code by 'cargo fmt'

--- src/rules/remove_if_expression.rs
+++ src/rules/remove_if_expression.rs
@@ -1,14 +1,6 @@
 use crate::nodes::{
-    self,
-    Block,
-    Expression,
-    FunctionCall,
-    IfBranch,
-    Prefix,
-    ReturnStatement,
-    IfStatement,
-    FunctionExpression,
-    ParentheseExpression
+    self, BinaryExpression, Block, ElseIfExpressionBranch, Expression, IndexExpression,
+    ParentheseExpression, TableEntry, TableExpression,
 };
 use crate::process::{DefaultVisitor, NodeProcessor, NodeVisitor};
 use crate::rules::{
@@ -20,34 +12,72 @@ use super::verify_no_rule_properties;
 #[derive(Default)]
 struct Processor {}
 
+fn process(
+    condition: &Expression,
+    result: &Expression,
+    else_result: &Expression,
+    branches: &Vec<&ElseIfExpressionBranch>,
+) -> Expression {
+    let result_parenthese = ParentheseExpression::new(result.clone());
+    let result_table = TableExpression::new(vec![TableEntry::Value(result_parenthese.into())]);
+    let if_bin = BinaryExpression::new(nodes::BinaryOperator::And, condition.clone(), result_table);
+    let bin_right = if branches.len() > 0 {
+        let b = branches[0];
+        process(
+            b.get_condition(),
+            b.get_result(),
+            else_result,
+            &branches[1..].to_vec(),
+        )
+    } else {
+        let else_result_parenthese = ParentheseExpression::new(else_result.clone());
+        let else_result_table =
+            TableExpression::new(vec![TableEntry::Value(else_result_parenthese.into())]);
+        else_result_table.into()
+    };
+    let bin = BinaryExpression::new(nodes::BinaryOperator::Or, if_bin, bin_right);
+    let bin_parenthese = ParentheseExpression::new(Expression::Binary(Box::new(bin)));
+    IndexExpression::new(bin_parenthese, 1).into()
+}
+
 impl NodeProcessor for Processor {
     fn process_expression(&mut self, expression: &mut Expression) {
-        let call_exp: Option<Expression> = if let Expression::If(if_exp) = expression {
-            let result_return = ReturnStatement::one(if_exp.get_result().clone());
-            let else_result_return = ReturnStatement::one(if_exp.get_else_result().clone());
-            let front_branch = IfBranch::new(if_exp.get_condition().clone(), result_return);
-            let else_block = Block::new(vec![], Some(else_result_return.into()));
-
-            let mut branches: Vec<IfBranch> = vec![front_branch];
-            for elseif_exp in if_exp.iter_branches() {
-                let elseif_result_return = ReturnStatement::one(elseif_exp.get_result().clone());
-                let elseif_block = Block::new(vec![], Some(elseif_result_return.into()));
-                branches.push(IfBranch::new(elseif_exp.get_condition().clone(), elseif_block));
-            }
-
-            let r#if = IfStatement::new(branches, Some(else_block));
-
-            let func_block = Block::new(vec![r#if.into()], None);
-            let func = Expression::Function(FunctionExpression::from_block(func_block));
-            let parenthese_func = Prefix::Parenthese(ParentheseExpression::new(func));
-            let func_call = FunctionCall::from_prefix(parenthese_func);
-            Some(Expression::Call(Box::new(func_call)))
-        } else {
-            None
-        };
-        if let Some(exp) = call_exp {
-            *expression = exp;
+        if let Expression::If(if_exp) = expression {
+            let translated_exp = process(
+                if_exp.get_condition(),
+                if_exp.get_result(),
+                if_exp.get_else_result(),
+                &if_exp.iter_branches().collect(),
+            );
+            *expression = translated_exp;
         }
+
+        // let call_exp: Option<Expression> = if let Expression::If(if_exp) = expression {
+        //     let result_return = ReturnStatement::one(if_exp.get_result().clone());
+        //     let else_result_return = ReturnStatement::one(if_exp.get_else_result().clone());
+        //     let front_branch = IfBranch::new(if_exp.get_condition().clone(), result_return);
+        //     let else_block = Block::new(vec![], Some(else_result_return.into()));
+
+        //     let mut branches: Vec<IfBranch> = vec![front_branch];
+        //     for elseif_exp in if_exp.iter_branches() {
+        //         let elseif_result_return = ReturnStatement::one(elseif_exp.get_result().clone());
+        //         let elseif_block = Block::new(vec![], Some(elseif_result_return.into()));
+        //         branches.push(IfBranch::new(elseif_exp.get_condition().clone(), elseif_block));
+        //     }
+
+        //     let r#if = IfStatement::new(branches, Some(else_block));
+
+        //     let func_block = Block::new(vec![r#if.into()], None);
+        //     let func = Expression::Function(FunctionExpression::from_block(func_block));
+        //     let parenthese_func = Prefix::Parenthese(ParentheseExpression::new(func));
+        //     let func_call = FunctionCall::from_prefix(parenthese_func);
+        //     Some(Expression::Call(Box::new(func_call)))
+        // } else {
+        //     None
+        // };
+        // if let Some(exp) = call_exp {
+        //     *expression = exp;
+        // }
     }
 }
 
