2ce7af1964b59f41a84a6fe7102ad0ac49359419
jiwonz <jiwonz0113@gmail.com>
2024-09-03 21:39:43 +0900

    Remove disabled codes which were comments

--- src/rules/remove_continue.rs
+++ src/rules/remove_continue.rs
@@ -16,115 +16,10 @@ use hex;
 
 const BREAK_VARIABLE_NAME: &str = "__DARKLUA_REMOVE_CONTINUE_break";
 
-/// deeply searches for blocks and checks if LastStatement exists
-// fn last_stmt_exists(block: &mut Block) -> Option<&mut Block> {
-//     if let Some(_) = block.get_last_statement() {
-//         return Some(block);
-//     }
-//     for stmt in block.iter_mut_statements() {
-//         match stmt {
-//             Statement::If(if_stmt) => {
-//                 for v in if_stmt.mutate_branches() {
-//                     if let Some(b) = last_stmt_exists(v.mutate_block()) {
-//                         return Some(b);
-//                     }
-//                 }
-//                 return None;
-//             }
-//             Statement::Repeat(repeat) => {
-//                 if let Some(b) = last_stmt_exists(repeat.mutate_block()) {
-//                     return Some(b);
-//                 }
-//             }
-//             _ => {
-//                 return None;
-//             }
-//         }
-//     }
-//     None
-// }
-
 #[derive(Default)]
 struct Processor {
     break_variable_name: String,
 }
-
-// impl NodeProcessor for Processor {
-//     fn process_statement(&mut self, statement: &mut nodes::Statement) {
-//         match statement {
-//             Statement::NumericFor(numeric_for) => {
-//                 let numeric_for2 = numeric_for.clone();
-//                 let original_block = numeric_for.mutate_block();
-//                 let new_block = if let Some(block) = last_stmt_exists(original_block) {
-//                     if !matches!(block.get_last_statement(), Some(LastStatement::Continue(_))) {
-//                         return
-//                     }
-//                     let repeat = RepeatStatement::new(numeric_for2.get_block().clone(), true);
-//                     let repeat_last_stmt = if let Some(s) = numeric_for2.get_block().get_last_statement() {
-//                         Some(s.clone())
-//                     } else {
-//                         None
-//                     };
-//                     Some(Block::new(vec![repeat.into()], repeat_last_stmt))
-//                 } else {
-//                     None
-//                 };
-//                 if let Some(b) = new_block {
-//                     *original_block = b;
-//                     if let Some(block) = last_stmt_exists(original_block) {
-//                         block.set_last_statement(LastStatement::new_break());
-//                     }
-//                 }
-//             },
-//             _ => {}
-//         }
-//     }
-// }
-
-// struct CollectedStatement {
-//     block: Block,
-//     last_statement: LastStatement,
-// }
-
-// fn collect_last_statements(block: &mut Block, result: &mut Vec<CollectedStatement>) {
-//     if let Some(last_stmt) = block.get_last_statement() {
-//         result.push(CollectedStatement {
-//             block: block.clone(),
-//             last_statement: last_stmt.clone(),
-//         });
-//     }
-//     for stmt in block.iter_mut_statements() {
-//         match stmt {
-//             Statement::If(if_stmt) => {
-//                 for b in if_stmt.mutate_all_blocks() {
-//                     collect_last_statements(b, result);
-//                 }
-//             }
-//             _ => {}
-//         }
-//     }
-// }
-
-// fn collect_continue_break_blocks<'a>(block: &mut Block) -> Vec<&mut Block> {
-// 	let mut collected = Vec::new();
-// 	if let Some(last_stmt) = block.get_last_statement() {
-// 		if matches!(last_stmt, LastStatement::Continue(_))
-// 			|| matches!(last_stmt, LastStatement::Break(_)) {
-// 			collected.push(block);
-// 		}
-// 	}
-// 	for stmt in block.iter_mut_statements() {
-// 		match stmt {
-// 			Statement::If(if_stmt) => {
-// 				for branch in if_stmt.mutate_branches().iter_mut() {
-// 					collected.extend(collect_continue_break_blocks(branch.mutate_block()));
-// 				}
-// 			},
-// 			_ => {}
-// 		}
-// 	}
-// 	collected
-// }
 
 fn continue_break_exists(block: &Block) -> (bool, bool) {
     let (mut has_continue, mut has_break) = if let Some(last_stmt) = block.get_last_statement() {
@@ -159,24 +54,24 @@ fn continue_break_exists(block: &Block) -> (bool, bool) {
 }
 
 fn continues_to_breaks(block: &mut Block) {
-	if let Some(last_stmt) = block.mutate_last_statement() {
-		if matches!(last_stmt, LastStatement::Continue(_)) {
-			*last_stmt = LastStatement::new_break();
-		}
-	}
-	for stmt in block.iter_mut_statements() {
-		match stmt {
-			Statement::If(if_stmt) => {
-				for branch in if_stmt.mutate_branches().iter_mut() {
-					continues_to_breaks(branch.mutate_block());
-				}
-			},
-			Statement::Do(do_stmt) => {
-				continues_to_breaks(do_stmt.mutate_block());
-			},
-			_ => {}
-		}
-	}
+    if let Some(last_stmt) = block.mutate_last_statement() {
+        if matches!(last_stmt, LastStatement::Continue(_)) {
+            *last_stmt = LastStatement::new_break();
+        }
+    }
+    for stmt in block.iter_mut_statements() {
+        match stmt {
+            Statement::If(if_stmt) => {
+                for branch in if_stmt.mutate_branches().iter_mut() {
+                    continues_to_breaks(branch.mutate_block());
+                }
+            }
+            Statement::Do(do_stmt) => {
+                continues_to_breaks(do_stmt.mutate_block());
+            }
+            _ => {}
+        }
+    }
 }
 
 fn continues_with_breaks_to_breaks(block: &mut Block, break_variable_name: &str) {
@@ -214,15 +109,6 @@ impl NodeProcessor for Processor {
         match statement {
             Statement::NumericFor(numeric_for) => {
                 let block = numeric_for.mutate_block();
-                // let last_stmts = &mut Vec::new();
-                // collect_last_statements(block, last_stmts);
-
-                // let has_continue = last_stmts
-                //     .iter()
-                //     .any(|cs| matches!(cs.last_statement, LastStatement::Continue(_)));
-                // let has_break = last_stmts
-                //     .iter()
-                //     .any(|cs| matches!(cs.last_statement, LastStatement::Break(_)));
 
                 let (has_continue, has_break) = continue_break_exists(block);
 
@@ -245,11 +131,6 @@ impl NodeProcessor for Processor {
                     };
                     let repeat_stmt = RepeatStatement::new(block.clone(), true);
                     stmts.push(repeat_stmt.into());
-                    // let last_stmt = if let Some(l) = block.mutate_last_statement() {
-                    //     Some(l.clone())
-                    // } else {
-                    //     None
-                    // };
                     if let Some(break_variable_handler) = break_variable_handler {
                         stmts.push(break_variable_handler.into());
                     }
@@ -261,24 +142,9 @@ impl NodeProcessor for Processor {
     }
 }
 
-// fn process(block: &mut Block) {
-
-// }
-
-// impl NodeProcessor for Processor {
-// 	fn process_statement(&mut self, statement: &mut Statement) {
-// 		match statement {
-// 			Statement::NumericFor(numeric_for) => {
-
-// 			},
-// 			_ => {}
-// 		}
-// 	}
-// }
-
 pub const REMOVE_CONTINUE_RULE_NAME: &str = "remove_continue";
 
-/// A rule that removes trailing `nil` in local assignments.
+/// A rule that removes continue statements and convert into breaks.
 #[derive(Debug, Default, PartialEq, Eq)]
 pub struct RemoveContinue {}
 
