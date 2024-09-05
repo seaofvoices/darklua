960bb37a984deaa8dd6982b3fcc2549c21a059f0
jiwonz <jiwonz0113@gmail.com>
2024-09-05 14:35:36 +0900

    Add optimization based on continue and break count

--- src/rules/remove_continue.rs
+++ src/rules/remove_continue.rs
@@ -2,7 +2,8 @@ use std::fmt::Debug;
 
 use crate::nodes::{
     AssignStatement, Block, Expression, Identifier, IfStatement, LastStatement,
-    LocalAssignStatement, RepeatStatement, Statement, TypedIdentifier, Variable,
+    LocalAssignStatement, RepeatStatement, Statement, TypedIdentifier, UnaryExpression,
+    UnaryOperator, Variable,
 };
 use crate::process::{DefaultVisitor, NodeProcessor, NodeVisitor};
 use crate::rules::{
@@ -15,42 +16,50 @@ use blake3;
 use hex;
 
 const BREAK_VARIABLE_NAME: &str = "__DARKLUA_REMOVE_CONTINUE_break";
+const CONTINUE_VARIABLE_NAME: &str = "__DARKLUA_REMOVE_CONTINUE_continue";
 
 #[derive(Default)]
 struct Processor {
     break_variable_name: String,
+    continue_variable_name: String,
 }
 
-fn continue_break_exists(block: &Block) -> (bool, bool) {
-    let (mut has_continue, mut has_break) = if let Some(last_stmt) = block.get_last_statement() {
+fn count_continue_break(block: &Block) -> (usize, usize) {
+    let (mut continue_count, mut break_count) = if let Some(last_stmt) = block.get_last_statement()
+    {
         (
-            matches!(last_stmt, LastStatement::Continue(_)),
-            matches!(last_stmt, LastStatement::Break(_)),
+            if let LastStatement::Continue(_) = last_stmt {
+                1
+            } else {
+                0
+            },
+            if let LastStatement::Break(_) = last_stmt {
+                1
+            } else {
+                0
+            },
         )
     } else {
-        (false, false)
+        (0, 0)
     };
     for stmt in block.iter_statements() {
         match stmt {
             Statement::If(if_stmt) => {
                 for branch in if_stmt.iter_branches() {
-                    let (c, b) = continue_break_exists(branch.get_block());
-                    has_continue = has_continue || c;
-                    has_break = has_break || b;
+                    let (c, b) = count_continue_break(branch.get_block());
+                    continue_count = continue_count + c;
+                    break_count = break_count + b;
                 }
             }
             Statement::Do(do_stmt) => {
-                let (c, b) = continue_break_exists(do_stmt.get_block());
-                has_continue = has_continue || c;
-                has_break = has_break || b;
+                let (c, b) = count_continue_break(do_stmt.get_block());
+                continue_count = continue_count + c;
+                break_count = break_count + b;
             }
             _ => {}
         }
-        if has_continue && has_break {
-            break;
-        }
     }
-    (has_continue, has_break)
+    (continue_count, break_count)
 }
 
 fn continues_to_breaks(block: &mut Block) {
@@ -74,52 +83,33 @@ fn continues_to_breaks(block: &mut Block) {
     }
 }
 
-fn continues_with_breaks_to_breaks(block: &mut Block, break_variable_name: &str) {
-    if let Some(last_stmt) = block.mutate_last_statement() {
-        match last_stmt {
-            LastStatement::Continue(_) => {
-                block.set_last_statement(LastStatement::new_break());
-            }
-            LastStatement::Break(_) => {
-                let var = Variable::new(break_variable_name);
-                let break_var_assign_stmt = AssignStatement::from_variable(var, true);
-                block.push_statement(break_var_assign_stmt);
-                block.set_last_statement(LastStatement::new_break());
-            }
-            _ => {}
-        }
-    }
-    for stmt in block.iter_mut_statements() {
-        match stmt {
-            Statement::If(if_stmt) => {
-                for branch in if_stmt.mutate_branches().iter_mut() {
-                    continues_with_breaks_to_breaks(branch.mutate_block(), break_variable_name);
-                }
-            }
-            Statement::Do(do_stmt) => {
-                continues_with_breaks_to_breaks(do_stmt.mutate_block(), break_variable_name);
-            }
-            _ => {}
-        }
-    }
-}
-
 impl Processor {
     fn process(&self, block: &mut Block) {
-        let (has_continue, has_break) = continue_break_exists(block);
+        let (continue_count, break_count) = count_continue_break(block);
 
-        if has_continue {
-            let (mut stmts, break_variable_handler) = if has_break {
+        if continue_count > 0 {
+            let (mut stmts, break_variable_handler) = if break_count > 0 {
                 let var = TypedIdentifier::new(self.break_variable_name.as_str());
                 let value = Expression::False(None);
                 let local_assign_stmt = LocalAssignStatement::new(vec![var], vec![value]);
 
+                let with_continue_statement = continue_count < break_count;
                 let break_block = Block::new(vec![], Some(LastStatement::new_break()));
-                let break_variable_handler = IfStatement::create(
-                    Identifier::new(self.break_variable_name.as_str()),
-                    break_block,
-                );
-                continues_with_breaks_to_breaks(block, self.break_variable_name.as_str());
+                let break_variable_handler = if with_continue_statement {
+                    IfStatement::create(
+                        UnaryExpression::new(
+                            UnaryOperator::Not,
+                            Identifier::new(self.continue_variable_name.as_str()),
+                        ),
+                        break_block,
+                    )
+                } else {
+                    IfStatement::create(
+                        Identifier::new(self.break_variable_name.as_str()),
+                        break_block,
+                    )
+                };
+                self.continues_with_breaks_to_breaks(block, with_continue_statement);
                 (vec![local_assign_stmt.into()], Some(break_variable_handler))
             } else {
                 continues_to_breaks(block);
@@ -131,6 +121,52 @@ impl Processor {
                 stmts.push(break_variable_handler.into());
             }
             *block = Block::new(stmts, None);
+        }
+    }
+
+    fn continues_with_breaks_to_breaks(&self, block: &mut Block, with_continue_statement: bool) {
+        if let Some(last_stmt) = block.mutate_last_statement() {
+            let (continue_statement, break_statement) = if with_continue_statement {
+                let var = Variable::new(self.continue_variable_name.as_str());
+                (Some(AssignStatement::from_variable(var, true)), None)
+            } else {
+                let var = Variable::new(self.break_variable_name.as_str());
+                (None, Some(AssignStatement::from_variable(var, true)))
+            };
+            match last_stmt {
+                LastStatement::Continue(_) => {
+                    if let Some(stmt) = continue_statement {
+                        block.push_statement(stmt);
+                    }
+                    block.set_last_statement(LastStatement::new_break());
+                }
+                LastStatement::Break(_) => {
+                    if let Some(stmt) = break_statement {
+                        block.push_statement(stmt);
+                    }
+                    block.set_last_statement(LastStatement::new_break());
+                }
+                _ => {}
+            }
+        }
+        for stmt in block.iter_mut_statements() {
+            match stmt {
+                Statement::If(if_stmt) => {
+                    for branch in if_stmt.mutate_branches().iter_mut() {
+                        self.continues_with_breaks_to_breaks(
+                            branch.mutate_block(),
+                            with_continue_statement,
+                        );
+                    }
+                }
+                Statement::Do(do_stmt) => {
+                    self.continues_with_breaks_to_breaks(
+                        do_stmt.mutate_block(),
+                        with_continue_statement,
+                    );
+                }
+                _ => {}
+            }
         }
     }
 }
@@ -158,8 +194,10 @@ impl FlawlessRule for RemoveContinue {
         let hash = blake3::hash(format!("{block:?}").as_bytes());
         let hash_hex = hex::encode(&hash.as_bytes()[..8]);
         let break_variable_name = BREAK_VARIABLE_NAME.to_string() + hash_hex.as_str();
+        let continue_variable_name = CONTINUE_VARIABLE_NAME.to_string() + hash_hex.as_str();
         let mut processor = Processor {
             break_variable_name,
+            continue_variable_name,
         };
         DefaultVisitor::visit_block(block, &mut processor);
     }
