7eb420d6ff90d88a22aaaddb3c44d598f92ff658
jiwonz <jiwonz0113@gmail.com>
2024-09-27 13:49:27 +0900

    Add tests

--- src/rules/remove_generalized_iteration.rs
+++ src/rules/remove_generalized_iteration.rs
@@ -241,6 +241,7 @@ mod test {
         let result = json5::from_str::<Box<dyn Rule>>(
             r#"{
             rule: 'remove_generalized_iteration',
+			runtime_variable_format: '{name}',
             prop: "something",
         }"#,
         );
---
+++ tests/rule_tests/remove_generalized_iteration.rs
@@ -0,0 +1,24 @@
+use darklua_core::rules::Rule;
+
+test_rule!(
+    remove_generalized_iteration,
+    json5::from_str::<Box<dyn Rule>>(
+        r#"{
+            rule: 'remove_generalized_iteration',
+			runtime_variable_format: '{name}'
+        }"#
+    ).unwrap(),
+	generic_for("for i,v in {1,2,3} do end")
+		=> "do local iter,invar,control={1,2,3} if type(iter)=='table' then local _m=getmetatable(iter) if type(_m)=='table' and type(_m.__iter)=='function' then iter,invar,control=_m.__iter() else iter,invar,control=pairs(iter) end end for i,v in iter,invar,control do end end"
+);
+
+#[test]
+fn deserialize_from_object_notation() {
+    json5::from_str::<Box<dyn Rule>>(
+        r#"{
+        rule: 'remove_generalized_iteration',
+		runtime_variable_format: '{name}'
+    }"#,
+    )
+    .unwrap();
+}
