50ed9ef62452de048348eda0b96d2332439e8a2f
jiwonz <jiwonz0113@gmail.com>
2024-09-27 21:41:03 +0900

    Fix test

--- tests/rule_tests/remove_generalized_iteration.rs
+++ tests/rule_tests/remove_generalized_iteration.rs
@@ -9,7 +9,7 @@ test_rule!(
         }"#
     ).unwrap(),
     generic_for("for i,v in {1,2,3} do end")
-        => "do local iter,invar,control={1,2,3} if type(iter)=='table' then local _m=getmetatable(iter) if type(_m)=='table' and type(_m.__iter)=='function' then iter,invar,control=_m.__iter() else iter,invar,control=pairs(iter) end end for i,v in iter,invar,control do end end"
+        => "do local iter={1,2,3} local invar,control if type(iter)=='table' then local _m=getmetatable(iter) if type(_m)=='table' and type(_m.__iter)=='function' then iter,invar,control=_m.__iter() else iter,invar,control=pairs(iter) end end for i,v in iter,invar,control do end end"
 );
 
 #[test]
