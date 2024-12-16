0f013d7a722ceea4c0c29cf7c5580126bc9cae35
jeparlefrancais <jeparlefrancais21@gmail.com>
2024-12-16 14:06:13 -0500

    Track symlinks to watch the actual file changes

--- src/cli/utils/file_watcher.rs
+++ src/cli/utils/file_watcher.rs
@@ -1,6 +1,8 @@
 use std::{
     collections::HashSet,
     env,
+    hash::Hash,
+    iter,
     path::{Path, PathBuf},
     sync::mpsc::{self, Receiver, Sender},
     time::Duration,
@@ -22,12 +24,14 @@ enum WatcherSignal {
 }
 
 pub struct FileWatcher {
+    input_path: PathBuf,
     resources: Resources,
     sender: Sender<WatcherSignal>,
     receiver: Option<Receiver<WatcherSignal>>,
     worker_tree: Option<WorkerTree>,
     process_option: ProcessOptions,
     extra_file_watch: HashSet<PathBuf>,
+    links_file_watch: HashSet<(PathBuf, PathBuf)>,
     current_working_path: Option<PathBuf>,
 }
 
@@ -36,12 +40,14 @@ impl FileWatcher {
         let (sender, receiver) = mpsc::channel();
 
         Self {
+            input_path: process_option.input_path.clone(),
             resources: Resources::from_file_system(),
             sender,
             receiver: Some(receiver),
             worker_tree: None,
             process_option: process_option.clone(),
             extra_file_watch: Default::default(),
+            links_file_watch: Default::default(),
             current_working_path: env::current_dir().ok(),
         }
     }
@@ -73,8 +79,16 @@ impl FileWatcher {
             .take()
             .expect("file watcher channel receiver should exist");
 
-        let input_path = self.process_option.input_path.clone();
+        let input_path = self.input_path.clone();
         let config_path = self.process_option.config.clone();
+
+        for link_path in iter_all_links(input_path.clone()) {
+            if let Ok(link_location) = link_path.read_link() {
+                self.send_watch_signal(&link_location);
+
+                self.links_file_watch.insert((link_location, link_path));
+            }
+        }
 
         let mut debouncer = new_debouncer(
             Duration::from_millis(FILE_WATCHING_DEBOUNCE_DURATION_MILLIS),
@@ -148,7 +162,8 @@ impl FileWatcher {
             match receiver.recv().expect("Could not receive from channel.") {
                 WatcherSignal::Exit => break,
                 WatcherSignal::Watch(path) => {
-                    match debouncer.watch(&path, RecursiveMode::NonRecursive) {
+                    log::debug!("start file watching on '{}'", path.display());
+                    match debouncer.watch(&path, RecursiveMode::Recursive) {
                         Ok(()) => {}
                         Err(err) => {
                             log::error!(
@@ -159,20 +174,46 @@ impl FileWatcher {
                         }
                     }
                 }
-                WatcherSignal::Unwatch(path) => match debouncer.unwatch(&path) {
-                    Ok(()) => {}
-                    Err(err) => {
-                        log::error!(
-                            "unable to stop watching file system at `{}`: {}",
-                            path.display(),
-                            err
-                        );
+                WatcherSignal::Unwatch(path) => {
+                    log::debug!("stop file watching on '{}'", path.display());
+                    match debouncer.unwatch(&path) {
+                        Ok(()) => {}
+                        Err(err) => {
+                            log::error!(
+                                "unable to stop watching file system at `{}`: {}",
+                                path.display(),
+                                err
+                            );
+                        }
                     }
-                },
+                }
             }
         }
 
         Ok(())
+    }
+
+    fn send_watch_signal(&self, link_location: &Path) {
+        if let Err(err) = self
+            .sender
+            .send(WatcherSignal::Watch(link_location.to_path_buf()))
+        {
+            log::warn!(
+                "unable to send signal to watch '{}': {}",
+                link_location.display(),
+                err
+            );
+        }
+    }
+
+    fn send_unwatch_signal(&self, path: &Path) {
+        if let Err(err) = self.sender.send(WatcherSignal::Unwatch(path.to_path_buf())) {
+            log::warn!(
+                "unable to send signal to unwatch '{}': {}",
+                path.display(),
+                err
+            );
+        }
     }
 
     fn setup_ctrl_exit(&self) -> Result<(), CliError> {
@@ -206,9 +247,18 @@ impl FileWatcher {
         let mut has_created = false;
 
         for event in events {
+            let links = &self.links_file_watch;
+
             let mut paths_iterator = event.event.paths.iter().map(|path| {
-                current_path
-                    .and_then(|current_path| path.strip_prefix(current_path).ok())
+                links
+                    .iter()
+                    .find_map(|(link_location, link_path)| {
+                        path.starts_with(link_location)
+                            .then_some(link_path.as_path())
+                    })
+                    .or_else(|| {
+                        current_path.and_then(|current_path| path.strip_prefix(current_path).ok())
+                    })
                     .unwrap_or(path)
             });
 
@@ -253,6 +303,7 @@ impl FileWatcher {
 
         if has_created {
             self.worker_collect_work();
+            self.update_links();
         }
     }
 
@@ -270,37 +321,116 @@ impl FileWatcher {
                 .map(ToOwned::to_owned)
                 .collect();
 
-            for last_file in self.extra_file_watch.difference(&files) {
-                log::debug!("stop file watching on '{}'", last_file.display());
-                if let Err(err) = self
-                    .sender
-                    .send(WatcherSignal::Unwatch(last_file.to_path_buf()))
-                {
-                    log::warn!(
-                        "unable to send signal to unwatch '{}': {}",
-                        last_file.display(),
-                        err
-                    );
-                }
-            }
-
-            for new_file in files.difference(&self.extra_file_watch) {
-                log::debug!("start file watching on '{}'", new_file.display());
-                if let Err(err) = self
-                    .sender
-                    .send(WatcherSignal::Watch(new_file.to_path_buf()))
-                {
-                    log::warn!(
-                        "unable to send signal to watch '{}': {}",
-                        new_file.display(),
-                        err
-                    );
-                }
-            }
+            diff_sets(
+                &files,
+                &self.extra_file_watch,
+                |new_file| {
+                    self.send_watch_signal(new_file);
+                },
+                |last_file| {
+                    self.send_unwatch_signal(last_file);
+                },
+            );
 
             self.extra_file_watch = files;
         }
     }
+
+    fn update_links(&mut self) {
+        let new_links: HashSet<_> = iter_all_links(self.input_path.clone())
+            .filter_map(|link_path| {
+                link_path
+                    .read_link()
+                    .ok()
+                    .map(|link_location| (link_location, link_path))
+            })
+            .collect();
+
+        diff_sets(
+            &new_links,
+            &self.links_file_watch,
+            |(link_location, _link_path)| {
+                self.send_watch_signal(link_location);
+            },
+            |(link_location, _link_path)| {
+                self.send_unwatch_signal(link_location);
+            },
+        );
+
+        self.links_file_watch = new_links;
+    }
+}
+
+fn diff_sets<T: Eq + Hash>(
+    new_set: &HashSet<T>,
+    previous_set: &HashSet<T>,
+    on_added: impl Fn(&T),
+    on_removed: impl Fn(&T),
+) {
+    for item in previous_set.difference(new_set) {
+        on_removed(item);
+    }
+
+    for item in new_set.difference(previous_set) {
+        on_added(item);
+    }
+}
+
+fn iter_all_links(location: PathBuf) -> impl Iterator<Item = PathBuf> {
+    let mut unknown_paths = vec![location];
+    let mut links = Vec::new();
+    let mut dir_entries = Vec::new();
+
+    iter::from_fn(move || loop {
+        if let Some(location) = unknown_paths.pop() {
+            match location.symlink_metadata() {
+                Ok(metadata) => {
+                    if metadata.is_symlink() {
+                        links.push(location);
+                    } else if metadata.is_dir() {
+                        dir_entries.push(location.to_path_buf());
+                    };
+                }
+                Err(err) => {
+                    log::warn!(
+                        "unable to read metadata from file `{}`: {}",
+                        location.display(),
+                        err
+                    );
+                }
+            }
+        } else if let Some(dir_location) = dir_entries.pop() {
+            match dir_location.read_dir() {
+                Ok(read_dir) => {
+                    for entry in read_dir {
+                        match entry {
+                            Ok(entry) => {
+                                unknown_paths.push(entry.path());
+                            }
+                            Err(err) => {
+                                log::warn!(
+                                    "unable to read directory entry `{}`: {}",
+                                    dir_location.display(),
+                                    err
+                                );
+                            }
+                        }
+                    }
+                }
+                Err(err) => {
+                    log::warn!(
+                        "unable to read directory `{}`: {}",
+                        dir_location.display(),
+                        err
+                    );
+                }
+            }
+        } else if let Some(path) = links.pop() {
+            break Some(path);
+        } else {
+            break None;
+        }
+    })
 }
 
 fn log_darklua_error<T>(
