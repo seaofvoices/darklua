cc8713c5af492046271c7c3b69922ea581560efb
jeparlefrancais <jeparlefrancais21@gmail.com>
2026-02-24 15:02:10 -0500

    Release 0.18.0

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! darklua = "0.17.3"
+//! darklua = "0.18.0"
 //! ```
 //!
 //! This library is designed for developers who want to integrate Lua/Luau transformation capabilities
